4ec7d6ea01986df286572bb3c4f342479f6d0940
manish4umar <jimanish.official@gmail.com>
2025-10-16 20:15:35 +0530

    update

--- rect.cpp
+++ rect.cpp
@@ -1,61 +1,48 @@
 #include <iostream>
-#include <iomanip> 
+#include <iomanip>
 using namespace std;
 
 class rectangle
 {
 public:
-    double l, w;
-
-    void length()
+    double l;
+    double w;
+    void lenght()
     {
-        cout << "Enter Length of Rectangle: ";
+
+        cout << "Enter Length of Rectangle:- ";
         cin >> l;
     }
 
     void width()
     {
-        cout << "Enter Width of Rectangle: ";
+
+        cout << "Enter width of Rectangle:- ";
         cin >> w;
     }
 
     void area()
     {
-        cout << setw(20) << left << "Area of Rectangle:" 
-             << setw(10) << right << l * w << endl;
+        cout << endl<< "Area :- " << l * w << endl;
     }
 
     void perimeter()
     {
-        cout << setw(20) << left << "Perimeter of Rectangle:" 
-             << setw(10) << right << 2 * (l + w) << endl;
+        cout << "Perimeter :- " << 2 * (l + w) << endl;
     }
 };
 
 int main()
 {
     rectangle r;
-
-    cout << "------------------------------" << endl;
-    cout << "   RECTANGLE CALCULATOR" << endl;
-    cout << "------------------------------" << endl << endl;
-
-    r.length();
+    r.lenght();
     r.width();
+    cout << endl<<endl<<"Input the length of the rectangler: " << r.l << endl;
+    cout << "Input the width of the rectangle: " << r.w << endl<<endl;
+    ;
 
-    cout << "\n------------------------------" << endl;
-    cout << "      RECTANGLE DETAILS" << endl;
-    cout << "------------------------------" << endl;
-    cout << setw(20) << left << "Length:" << setw(10) << right << r.l << endl;
-    cout << setw(20) << left << "Width:" << setw(10) << right << r.w << endl;
-
-    cout << "\n------------------------------" << endl;
-    cout << "         CALCULATIONS" << endl;
-    cout << "------------------------------" << endl;
     r.area();
+
     r.perimeter();
-
-    cout << "------------------------------" << endl;
-
     return 0;
 }
